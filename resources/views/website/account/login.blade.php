dd1522d03df49893db2c5b8c9d85bdaf7d1958b9
Donaukinder <donaukinder@Tux.local>
2019-09-05 08:08:32 -0400

    Login 1/2

--- resources/views/website/account/login.blade.php
+++ resources/views/website/account/login.blade.php
@@ -1,1 +1,14 @@
-<H1>login</H1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/website.css')}}"/>
+    <title>Login</title>
+</head>
+<body>
+
+<script src="{{mix('js/website.js')}}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,5 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.copy(['resources/assets/image/website'], 'public/image/website')
+mix.styles(['resources/assets/css/website.css'], 'public/css/website.css')
+mix.js(['resources/assets/js/website.js'], 'public/js/website.js')
